c722626d360c741917c8872ed04aada7fc7bb193
Ashley Northup <northupashleya@gmail.com>
2023-02-03 13:31:03 -0500

    Implemented overhead clock in correct format.

--- Module 4/ViewController.swift
+++ Module 4/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var liveClock: UILabel!
+    
+    var Toptimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Toptimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.updateClock), userInfo: nil, repeats: true)
+        
     }
 
 
+    @objc func updateClock() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "E, d MMM yyyy HH:mm:ss"
+      
+        let date = Date()
+        
+        liveClock.text = dateFormatter.string(from: date)
+ 
+    }
+    
+    func changeBackground() {
+        
+            
+    }
+    
 }
 
