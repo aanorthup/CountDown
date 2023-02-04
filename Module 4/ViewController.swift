90e9b11285b9772302973f04421d13e925d24bd9
Ashley Northup <northupashleya@gmail.com>
2023-02-03 21:39:57 -0500

    added background functions and started button function

--- Module 4/ViewController.swift
+++ Module 4/ViewController.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
+extension Date {
+    var hour: Int { return Calendar.current.component(.hour, from: self) }
+}
+
 class ViewController: UIViewController {
 
 
     @IBOutlet weak var liveClock: UILabel!
     
+    @IBOutlet weak var timePicker: UIDatePicker!
+    
+    @IBOutlet weak var countdownTimer: UILabel!
+    
+    
     var Toptimer = Timer()
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,9 +30,17 @@ class ViewController: UIViewController {
         
         Toptimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.updateClock), userInfo: nil, repeats: true)
         
+        changeBackground()
+    
+        
+        
     }
 
-
+   
+    @IBAction func tappedButton(_ sender: Any) {
+        print(timePicker.countDownDuration.description)
+    }
+    
     @objc func updateClock() {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "E, d MMM yyyy HH:mm:ss"
@@ -33,8 +51,20 @@ class ViewController: UIViewController {
  
     }
     
-    func changeBackground() {
+    func scheduleBackground() {
+        timer = Timer(fireAt: Calendar.current.nextDate(after: Date(), matching: DateComponents(hour: 0..<12 ~= Date().hour ? 12 : 0), matchingPolicy: .nextTime)!, interval: 0, target: self, selector: #selector(changeBackground), userInfo: nil, repeats: false)
+        print(timer.fireDate)
+        RunLoop.main.add(timer, forMode: .common)
+        print("new background change scheduled at:", timer.fireDate.description(with: .current))
+    }
+    
+    @objc func changeBackground() {
+        //check for am or pm
+        self.view.backgroundColor = 0..<12 ~= Date().hour ? .yellow : .blue
         
+        //schedule timer
+        scheduleBackground()
+
             
     }
     
